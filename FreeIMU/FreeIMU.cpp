95b21c3ae6bcd754b99cc44e7699dcd32fc92646
ikeda <ikeda@koozyt.com>
2015-03-06 22:10:45 +0900

    Skip to adjust calibration when sensors are disabled

--- FreeIMU/FreeIMU.cpp
+++ FreeIMU/FreeIMU.cpp
@@ -312,9 +312,11 @@ void FreeIMU::getValues(float * values) {
     int16_t val[9];
     accgyromagn.getMotion9(&val[0], &val[1], &val[2], &val[3], &val[4], &val[5], &val[6], &val[7], &val[8]);
     // remove offsets from the gyroscope
-    val[3] = val[3] - gyro_off_x;
-    val[4] = val[4] - gyro_off_y;
-    val[5] = val[5] - gyro_off_z;
+    if ((val[3] != 0.0f) && (val[4] != 0.0f) && (val[5] != 0.0f)) {
+      val[3] = val[3] - gyro_off_x;
+      val[4] = val[4] - gyro_off_y;
+      val[5] = val[5] - gyro_off_z;
+    }
 
     for (int i=0; i<9; i++) {
       values[i] = (float) val[i];
@@ -349,9 +351,11 @@ void FreeIMU::getValues(float * values) {
 
   #warning Accelerometer calibration active: have you calibrated your device?
   // remove offsets and scale accelerometer (calibration)
-  values[0] = (values[0] - acc_off_x) / acc_scale_x;
-  values[1] = (values[1] - acc_off_y) / acc_scale_y;
-  values[2] = (values[2] - acc_off_z) / acc_scale_z;
+  if ((values[0] != 0.0f) && (values[1] != 0.0f) && (values[2] != 0.0f)) {
+    values[0] = (values[0] - acc_off_x) / acc_scale_x;
+    values[1] = (values[1] - acc_off_y) / acc_scale_y;
+    values[2] = (values[2] - acc_off_z) / acc_scale_z;
+  }
 
 
   #if HAS_HMC5883L()
